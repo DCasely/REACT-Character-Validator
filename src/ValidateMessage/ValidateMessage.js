a062a7a95f2299cdf83bb69fd5777eb9479e8e30
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-20 11:32:14 -0400

    renamed component and added styling

---
+++ src/ValidateMessage/ValidateMessage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ValidateMessage.css';
+
+function ValidateMessage(props) {
+  return <h2 className="ValidateMessage">{props.validate}</h2>;
+}
+
+export default ValidateMessage;
